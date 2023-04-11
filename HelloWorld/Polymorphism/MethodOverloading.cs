381ce76050bfc9ae5bb016a9cffb8c57466c1532
Harshita <chandaliyaharshita222@gmail.com>
2023-04-11 15:33:09 +0530

    Adding Practice Files

---
+++ HelloWorld/Polymorphism/MethodOverloading.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Polymorphism
+{
+    internal class Addition
+    {
+        public void AddNumbers(int first, int second)
+        {
+            Console.WriteLine("Sum of numbers: " + first + second);
+        }
+
+        //varrying number of parameters compared to 1st method
+        public void AddNumbers(int first, int second, int third)
+        {
+            Console.WriteLine("Sum of numbers: " + (first + second + third));
+        }
+
+        //changing datatype compared to 2nd method
+        public void AddNumbers(int first, double second, int third)
+        {
+            double res = first + second + third;
+            Console.WriteLine("Sum of numbers: " + res);
+        }
+
+        //changing order compared to 3rd method
+        public void AddNumbers(double second, int first, int third)
+        {
+            double res = first + second + third;
+            Console.WriteLine("Sum of numbers: " + res);
+        }
+    }
+}
---
+++ HelloWorld/Polymorphism/MethodOverriding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Polymorphism
+{
+    internal class Animal
+    {
+        internal string name;
+
+        public virtual void Run()
+        {
+            Console.WriteLine("Running slow");
+        }
+        public void AnimalDetails()
+        {
+            Console.WriteLine("Name of the animal: " + name);
+        }
+    }
+    class Lion:Animal
+    {
+        public override void Run()
+        {
+            Console.WriteLine("Running fast");
+        }
+    }
+}
